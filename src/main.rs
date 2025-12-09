3c6053c3017c276982ec10e9c8ffd4ec63750eb8
Polarice-d <thenextcomment2@gmail.com>
2025-12-09 15:05:45 -0600

    Added hound and basic .wav sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use hound;
+
 fn main() {
-    println!("Hello, world!");
+    let mut reader = hound::WavReader::open("samples/41khz_16bit.wav").unwrap();
+    let num = reader.samples::<i16>().count(); // I think this is the number of samples but i'm not sure
+    println!("{num}");
 }
